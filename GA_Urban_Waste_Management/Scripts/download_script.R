a18ebbf72a69bb0ad447ac61dfdf82c812b2d7bd
Siebring <jasper.siebring@wur.nl>
2017-06-07 16:07:43 +0200

    Added auto-download functionality for files <25mb - JS

---
+++ GA_Urban_Waste_Management/Scripts/download_script.R
@@ -0,0 +1,14 @@
+#Team Garbage/GW_Quality_Assessment_of_Infrastructure
+#6th of June 2017
+
+#Parses Google Drive links and downloads it to a specified folder
+#right click Google Drive files, select 'get shareable link', go to 'sharing settings' and copy the full URL
+#this method will only work for files <25mb, bigger files have to be downloaded manually
+
+fix = 'https://drive.google.com/uc?export=download&id='
+drive_download = function(dl_url, dest_name){
+  file_id = strsplit(dl_url, '/') 
+  file_id = file_id[[1]][6]
+  durl = paste0(fix, file_id)
+  download.file(durl, dest_name, mode= "wb")
+}
--- GA_Urban_Waste_Management/main.R
+++ GA_Urban_Waste_Management/main.R
@@ -1,17 +1,12 @@
-#Team Garbage
+#Team Garbage/GA_Urban_Waste_Management
+#6th of June 2017
 
-#REPLACE IF NEEDED, just a template
-#GA_Urban_Waste_Management
-#GW_Quality_Assessment_of_Infrastructure
-
-#time/data
+#set working directory to the same folder where main.R is saved!
 
 #library imports
-##
+source('Scripts/download_script.R')
 
-#data imports
-##
-
-
-#actual functionality 
-##
+## example/download data
+url = 'https://drive.google.com/file/d/0B2pH-wZjt_l4WlFnY2FKMXRic2c/view?usp=sharing'
+dest = './Data/Step/asphalt_points.csv'
+drive_download(url, dest)
