5711706fb09c3ca786433b2e0d1aa85c9063cded
Siebring <jasper.siebring@wur.nl>
2017-06-07 12:06:51 +0200

    ture

---
+++ GA_Urban_Waste_Management/Scripts/template.R
@@ -0,0 +1,17 @@
+#Team Garbage
+
+#REPLACE IF NEEDED, just a template
+#GA_Urban_Waste_Management
+#GW_Quality_Assessment_of_Infrastructure
+
+#time/data
+
+#library imports
+##
+
+#data imports
+##
+
+
+#actual functionality 
+##
---
+++ GW_Quality_Assessment_Of_Infrastructure/Data/Step/README.md
@@ -0,0 +1,2 @@
+# GRS60312_GR8
+Group Repository for the Remote Sensing and GIS Integration course
