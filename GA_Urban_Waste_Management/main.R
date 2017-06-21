bcd0293c650159e50b227aecc42bf817cc5f9985
Siebring <jasper.siebring@wur.nl>
2017-06-21 14:45:57 +0200

    Rewrote the main script based on our new DB (one connection, see comments for further changes) - JS

--- GA_Urban_Waste_Management/main.R
+++ GA_Urban_Waste_Management/main.R
@@ -1,7 +1,7 @@
 #GA_Urban_Waste_Management
 #Team Garbage
 
-#20th of June 2017
+#21st of June 2017
 
 #installs (and loads) required packages if they're not already found
 list.of.packages = c("rgdal", "RPostgreSQL", "sp", "rpostgis", "rstudioapi") 
@@ -13,45 +13,39 @@ lapply(list.of.packages, require, character.only=T)
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 
-
-
-
-
 #Setting the driver that'll connect to the PostgreSQL database
 drv = dbDriver("PostgreSQL")
 
 ###Connecting to the PostGreSQL database, 
-###1st line = local, 2th = WUR-network: PROJ_test, 3th = WUR-network:  GA_SWMS_database
-#con = dbConnect(drv, user = 'postgres', password = 'postgres', dbname = 'GA_SWMS_database') #localhost
-#con = dbConnect(drv, dbname ='PROJ_test', host = 'D0146435', port = 5432, user = 'postgres', password = 'postgres')
-#con = dbConnect(drv, dbname ='GA_SWMS_database', host = 'D0146435', port = 5432, user = 'postgres', password = 'postgres') 
+###1st line = localhost, 2th = WUR-network: SWMS_database 
+#con = dbConnect(drv, user = 'postgres', password = 'postgres', dbname = '')
+con = dbConnect(drv, dbname ='SWMS_database', host = 'D0146435', port = 5432, user = 'postgres', password = 'postgres') 
 
 
 ###EXTRACT DATA FROM DATABASE###
-#you can also add a query to the pgGetGeom function to directly filter spatial data
+#You can also add a query to the pgGetGeom function to directly filter spatial data
 #for spatial data:
-drains = pgGetGeom(con, name = c("workset333", "drains_table"), geom = "the_geom", gid = "gid") 
-network = pgGetGeom(con, name = c("workset333", "tbl_workset"), geom = "the_geom", gid = 'gid')
-
+sdf_network = pgGetGeom(con, name = c("public", "tbl_workset"), geom = "the_geom", gid = 'gid')
 #for plain tables:
-tbl_streets = dbReadTable(con, c("workset333", "tbl_streets"))
-tbl_navteq = dbReadTable(con, c("workset333", "tbl_navteq"))
+tbl_streets = dbReadTable(con, c("public", "tbl_streets"))
+tbl_navteq = dbReadTable(con, c("public", "tbl_navteq"))
 
 
 ###EXPORT DATA TO DATABASE###
-#Accepts both SpatialDataFrame and DataFrame
+#Accepts both SpatialDataFrame and DataFrame, specified table doesn't have to exist in DB
 #Specify the GEOM column if you're exporting spatial data
-#Specified table doesn't have to exist in the database
-pgInsert(con, name=c("workset333","drains_table"), data.obj= drains, geom = "the_geom", new.id = "gid")
-
+pgInsert(con, name=c("public","sdf_network"), data.obj= sdf_network, geom = "the_geom", new.id = "gid")
 
 ###RAW SQL QUERIES###
 #split up over three functions: 
-#   dbGetQuery(con, sql_string) #basic interactive use (use only for simple table select statements)
+#   dbGetQuery(con, sql_string) #basic interactive use (always use for table select statements otherwise you'll need to force a shutdown of the DB)
 #   dbExecute(con, sql_string) #interactive use, i.e. will return objects (use for more complex spatial queries) (sendstatement/getaffectedrows/clearresult in one function)
 #   dbSendQuery(con, sql_string) #only submits and executes the query (use for PGROUTING and SQL scripts)
 
+
+##If you want to store any spatialdataframes:
+#writeOGR(obj, dsn = './Export', driver = 'ESRI Shapefile', layer = 'network_layer')
+
 #disconnect connection
 dbDisconnect(con)
-
 
