68a6940e547347f1e6fa36c6233fa8ae14551fa8
Wiltenburg <bas.wiltenburg@wur.nl>
2017-06-30 12:27:57 +0200

    add truck properties, node bas

--- GA_Urban_Waste_Management/R/GA_a2_truck_properties.R
+++ GA_Urban_Waste_Management/R/GA_a2_truck_properties.R
@@ -1,7 +1,7 @@
 ##Script name :GA_a2_truck_properties.R
 ##Description :This script contains the (spatial) data frame of truck properties in Smart Waste Management System.
-#The output of this script is a table which becomes the project source for further analysis
-#The table is loaded to the central database using a connection to geodatabase server
+              #The output of this script is a table which becomes the project source for further analysis
+              #The table is loaded to the central database using a connection to geodatabase server
 ##Packages    : RPostgreSQL, sp, rpostgis
 ##Output table: GA_a2_truck_properties.R
 ##Attribute   : truck_license_plate, truck_type, truck_capacity, truck_speed, truck_emission, truck_location
@@ -27,7 +27,26 @@ truck_properties_table <- function(con, licenceplate, type, capacity, speed, emi
   
   #Insert spatial data frame to database
   TruckProperties_Geometry <- SpatialPointsDataFrame(coords = coordinates, data = TruckProperties,
+                                                   proj4string = CRS("+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0"))
+  pgInsert(con, name=c("public","a2_truck_properties"), data.obj= TruckProperties_Geometry, geom = "the_geom", new.id = "gid", overwrite = TRUE)
+  
+  a <- dbSendQuery(con, "CREATE OR REPLACE VIEW a2_linked_depot AS
+  SELECT DISTINCT ON(truck_license_plate) id, truck_license_plate, ST_Distance(a2_truck_properties.the_geom, pgnetwork_vertices_pgr.the_geom)
+  FROM a2_truck_properties RIGHT JOIN pgnetwork_vertices_pgr
+  ON ST_DWithin(a2_truck_properties.the_geom, pgnetwork_vertices_pgr.the_geom,0.001)
+  WHERE truck_license_plate IS NOT NULL ORDER BY truck_license_plate, st_distance ;")
+  
+  depottable = dbReadTable(con, c('public', 'a2_linked_depot'))
+  
+  depotNode <- as.numeric(depottable[1,1])
+  node <- c(depotNode)
+  
+  TruckProperties <- data.frame(truck_license_plate, truck_type, truck_capacity, truck_speed, truck_emission, node)  
+  TruckProperties_Geometry <- SpatialPointsDataFrame(coords = coordinates, data = TruckProperties,
                                                      proj4string = CRS("+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0"))
-  pgInsert(con, name=c("public","a2_truck_properties"), data.obj= TruckProperties_Geometry, geom = "the_geom", new.id = "gid")
+  
+  # Drob tables which are dependent on b4_bins_needs_to_be_emptied
+  b <- dbGetQuery(con, "DROP TABLE a2_truck_properties CASCADE ;")
+  pgInsert(con, name=c("public","a2_truck_properties"), data.obj= TruckProperties_Geometry, geom = "the_geom", new.id = "gid", overwrite = TRUE)
   
 }
